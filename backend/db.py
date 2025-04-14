468b559ec77c424a0821c7e0f22ce5a182dbe9cd
calleb-byte <122379645+calleb-byte@users.noreply.github.com>
2025-04-14 22:39:20 +0300

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -5,7 +5,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="yourpassword",
+        password="riga",
         database="invoiceauthentication"
     )
 
