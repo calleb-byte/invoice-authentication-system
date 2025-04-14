4a8e28d7283af3ae3114afa3e9a5adab957ebf62
calleb-byte <122379645+calleb-byte@users.noreply.github.com>
2025-04-14 22:35:18 +0300

    Update blockchain.py

--- backend/blockchain.py
+++ backend/blockchain.py
@@ -4,8 +4,8 @@ from config import INFURA_URL, CONTRACT_ADDRESS, ABI
 
 web3 = Web3(Web3.HTTPProvider(INFURA_URL))
 contract = web3.eth.contract(address=CONTRACT_ADDRESS, abi=ABI)
-sender_address = "YOUR_WALLET_ADDRESS"
-private_key = "YOUR_PRIVATE_KEY"
+sender_address = "0x9123317C21c16801123BAD11F7e9fe1D5769CBd5"
+private_key = "0x84354beca42fdcb10c8b41c4d995a5310040a29beae7f30f607a4ee5b597ea23"
 
 def record_invoice_on_chain(data):
     nonce = web3.eth.get_transaction_count(sender_address)
