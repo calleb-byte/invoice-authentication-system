5fc83c0a675c63543868b58278182528669c7654
calleb-byte <122379645+calleb-byte@users.noreply.github.com>
2025-04-14 22:33:23 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 
-INFURA_URL = "https://goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID"
-CONTRACT_ADDRESS = "YOUR_DEPLOYED_CONTRACT_ADDRESS"
+INFURA_URL = "https://mainnet.infura.io/v3/1bf182181f0147939b58df2012ffc249"
+CONTRACT_ADDRESS = "0x5c4B3fC5B79c6cfeD886bc8BD0b6Fd72DA4165CF"
 ABI = [
     {
         "inputs": [
